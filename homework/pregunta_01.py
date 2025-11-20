06dbe2ee91149789da472344aff08bd294961430
Ancarrenv <acarrenov@unal.edu.co>
2025-11-20 10:29:37 -0500

    LAB/06
    Terminado

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -13,3 +13,42 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    import os
+    import pandas as pd
+
+    
+    df = pd.read_csv("files/input/solicitudes_de_credito.csv", sep=";", index_col=0)
+    
+    df.dropna(inplace=True) 
+    df.drop_duplicates(inplace=True) 
+ 
+    df.fecha_de_beneficio = (pd.to_datetime(
+        df.fecha_de_beneficio, format="%d/%m/%Y", errors="coerce")
+        .combine_first(pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce")))
+
+    
+    df.sexo = df.sexo.str.lower().replace(['-', '_'], ' ', regex=True).str.strip()
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower().replace(['-', '_'], ' ', regex=True).str.strip()
+    df.idea_negocio = df.idea_negocio.str.lower().replace(['-', '_'], ' ', regex=True).str.strip()
+    df.barrio = df.barrio.str.lower().str.replace("-", " ", regex=False).str.replace("_", " ", regex=False)
+    df.línea_credito = df.línea_credito.str.lower().replace(['-', '_'], ' ', regex=True).str.strip()
+    df.estrato = df.estrato.astype(int)
+    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+
+    
+    df.monto_del_credito = df.monto_del_credito.str.strip().str.replace("$", "", regex=False).str.replace(",", "", regex=False).str.replace(".00", "", regex=False)
+    df.monto_del_credito = pd.to_numeric(df['monto_del_credito'], errors="coerce")
+
+    
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+
+    
+    os.makedirs("files/output", exist_ok=True)
+    df.to_csv("files/output/solicitudes_de_credito.csv", sep=";", index=False)
+
+pregunta_01()
+
+
+if __name__ == "__main__":
+    pregunta_01()
